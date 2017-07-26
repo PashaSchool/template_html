653d6455a8138737465229a6917afa8662b14ccd
Pasha School <pasha.school888@gmail.com>
2017-07-26 17:29:35 +0300

    nothing

--- app/js/main.js
+++ app/js/main.js
@@ -135,76 +135,6 @@ var contactUsForm = {
   
 };
 
-var serviceAnimation = {
-  init: function() {
-    this.cacheDom();
-    this.bindEvents();
-  },
-  cacheDom: function() {
-    this.$service = $('#service-section');
-    this.$title = $(this.$service).find('.top-header');
-    this.$circleCont = $(this.$service).find('.circle-container');
-
-    this.$item_1 = $(this.$circleCont).find('.circle-container__item:nth-child(1)');
-    this.$item_2 = $(this.$circleCont).find('.circle-container__item:nth-child(2)');
-    this.$item_3 = $(this.$circleCont).find('.circle-container__item:nth-child(3)');
-    this.$item_4 = $(this.$circleCont).find('.circle-container__item:nth-child(4)');
-    this.$item_5 = $(this.$circleCont).find('.circle-container__item:nth-child(5)');
-    this.$item_6 = $(this.$circleCont).find('.circle-container__item:nth-child(6)');
-
-    this.$c5 = $(this.$service).find('.circle-container__generate-circle-5');
-    this.$c4 = $(this.$service).find('.circle-container__generate-circle-4');
-    this.$c3 = $(this.$service).find('.circle-container__generate-circle-3');
-
-    this.$btn = $(this.$service).find('.btn-wrapper');
-
-    // offset top
-    this.offsetTop = $(this.$service).offset().top;
-    //timeline
-    this.timelineHeader = new TimelineLite({
-      paused: true,
-      onComplete: this._deleteALlAttrStyle.bind(this)
-    });
-
-  },
-  _deleteALlAttrStyle: function() {
-    $(this.$circleCont)
-    .children().each(function(i, el) {
-      if(el.hasAttribute('style')) {
-        $(el).removeAttr('style')
-      }
-    })
-  },
-  bindEvents: function() {
-    if($(window).width() > 1200) {
-      this.animation();
-      $(window).on('scroll', this.checkPosition.bind(this))
-    }
-  },
-  checkPosition: function(){
-    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-
-    if(scrolled >= (this.offsetTop * 0.9) ) {
-      this.timelineHeader.play();
-    }
-  },
-  animation: function() {
-    this.timelineHeader
-    .from(this.$title, 1, {autoAlpha: 0, y: -50, ease: Power2.easeOut})
-    .from(this.$c4, 5, {rotationZ: 360, ease: Power2.easeOut}, 'atOneTime')
-    .from(this.$c3, 4, {rotationZ: 360, ease: Power2.easeOut}, 'atOneTime')
-    .from(this.$c5, 4, {rotationZ: 360, ease: Power2.easeOut}, 'atOneTime')
-    .from(this.$item_1, 5, {autoAlpha: 0, x: 250, ease: Power2.easeOut}, 'atOneTime')
-    .from(this.$item_2, 5, {autoAlpha: 0, x: 250, ease: Power2.easeOut}, 'atOneTime')
-    .from(this.$item_6, 5, {autoAlpha: 0, x: 250, ease: Power2.easeOut}, 'atOneTime')
-    .from(this.$item_3, 5, {autoAlpha: 0, x: -500, ease: Power2.easeOut}, 'atOneTime')
-    .from(this.$item_4, 5, {autoAlpha: 0, x: -450, ease: Power2.easeOut}, 'atOneTime')
-    .from(this.$item_5, 5, {autoAlpha: 0, x: -500, ease: Power2.easeOut}, 'atOneTime')
-    .from(this.$btn, 4, {autoAlpha: 0, y: 100, ease: Power2.easeOut}, 'atOneTime')
-
-  }
-}
-
 var footerAnimation = {
   init: function() {
     this.chacheDom();
@@ -275,51 +205,6 @@ var headerAnimation = {
     .to(this.$arrow, 0.6, {y: 0, ease: Power2.easeOut})
   }
 }
-
-var navigationAction = {
-  init: function() {
-    this.chacheDom();
-    this.bindEvents();
-  },
-  chacheDom: function() {
-    this.btn = $('.burger-menu');
-    this.menu = $('.header-line__list');
-    this.windowWidth = $(window).innerWidth();
-    this.newClass = 'toggle-nav-on-mobile';
-  },
-  bindEvents: function() {
-    $(this.btn).on('click', this.addClass.bind(this));
-    $(window).on('resize', this.checkForMobileDevice.bind(this));
-    $(window).on('load', this.checkForMobileDevice.bind(this));
-    $(document).on('scroll', this.navigationFixed.bind(this));
-  },
-  navigationFixed: function(e) {
-    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-    var curentPos = $(window).height() * 1.5;
-    if(scrolled  >= curentPos) {
-      $('nav.navigation').addClass('menu-fixed')
-    }
-    if(scrolled <= curentPos && $('nav.navigation').hasClass('menu-fixed')) {
-      $('nav.navigation').removeClass('menu-fixed')
-    }
-  },
-  checkForMobileDevice: function() {
-    this.windowWidth = $(window).innerWidth();
-    if( $(this.menu).hasClass(this.newClass) && (this.windowWidth >= 979)) {
-      $(this.menu).removeClass(this.newClass);
-    }
-  },
-  addClass: function() {
-    $(this.menu).toggleClass(this.toggleClassCb.bind(this))
-  },
-  toggleClassCb: function() {
-    if(this.windowWidth <= 979) {
-      return this.newClass
-    } else {
-      return
-    }
-  }
-};
 
 function initMap() {
   var option = {
@@ -624,3 +509,117 @@ $(window).on('scroll', function() {
    }
   // console.log(document.querySelector('#portfolio-wrapper').offsetTop, window.pageYOffset)
 })
+var navigationAction = {
+  init: function() {
+    this.chacheDom();
+    this.bindEvents();
+  },
+  chacheDom: function() {
+    this.btn = $('.burger-menu');
+    this.menu = $('.header-line__list');
+    this.windowWidth = $(window).innerWidth();
+    this.newClass = 'toggle-nav-on-mobile';
+  },
+  bindEvents: function() {
+    $(this.btn).on('click', this.addClass.bind(this));
+    $(window).on('resize', this.checkForMobileDevice.bind(this));
+    $(window).on('load', this.checkForMobileDevice.bind(this));
+    $(document).on('scroll', this.navigationFixed.bind(this));
+  },
+  navigationFixed: function(e) {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    var curentPos = $(window).height() * 1.5;
+    if(scrolled  >= curentPos) {
+      $('nav.navigation').addClass('menu-fixed')
+    }
+    if(scrolled <= curentPos && $('nav.navigation').hasClass('menu-fixed')) {
+      $('nav.navigation').removeClass('menu-fixed')
+    }
+  },
+  checkForMobileDevice: function() {
+    this.windowWidth = $(window).innerWidth();
+    if( $(this.menu).hasClass(this.newClass) && (this.windowWidth >= 979)) {
+      $(this.menu).removeClass(this.newClass);
+    }
+  },
+  addClass: function() {
+    $(this.menu).toggleClass(this.toggleClassCb.bind(this))
+  },
+  toggleClassCb: function() {
+    if(this.windowWidth <= 979) {
+      return this.newClass
+    } else {
+      return
+    }
+  }
+};
+
+var serviceAnimation = {
+  init: function() {
+    this.cacheDom();
+    this.bindEvents();
+  },
+  cacheDom: function() {
+    this.$service = $('#service-section');
+    this.$title = $(this.$service).find('.top-header');
+    this.$circleCont = $(this.$service).find('.circle-container');
+
+    this.$item_1 = $(this.$circleCont).find('.circle-container__item:nth-child(1)');
+    this.$item_2 = $(this.$circleCont).find('.circle-container__item:nth-child(2)');
+    this.$item_3 = $(this.$circleCont).find('.circle-container__item:nth-child(3)');
+    this.$item_4 = $(this.$circleCont).find('.circle-container__item:nth-child(4)');
+    this.$item_5 = $(this.$circleCont).find('.circle-container__item:nth-child(5)');
+    this.$item_6 = $(this.$circleCont).find('.circle-container__item:nth-child(6)');
+
+    this.$c5 = $(this.$service).find('.circle-container__generate-circle-5');
+    this.$c4 = $(this.$service).find('.circle-container__generate-circle-4');
+    this.$c3 = $(this.$service).find('.circle-container__generate-circle-3');
+
+    this.$btn = $(this.$service).find('.btn-wrapper');
+
+    // offset top
+    this.offsetTop = $(this.$service).offset().top;
+    //timeline
+    this.timelineHeader = new TimelineLite({
+      paused: true,
+      onComplete: this._deleteALlAttrStyle.bind(this)
+    });
+
+  },
+  _deleteALlAttrStyle: function() {
+    $(this.$circleCont)
+    .children().each(function(i, el) {
+      if(el.hasAttribute('style')) {
+        $(el).removeAttr('style')
+      }
+    })
+  },
+  bindEvents: function() {
+    if($(window).width() > 1200) {
+      this.animation();
+      $(window).on('scroll', this.checkPosition.bind(this))
+    }
+  },
+  checkPosition: function(){
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+    if(scrolled >= (this.offsetTop * 0.9) ) {
+      this.timelineHeader.play();
+    }
+  },
+  animation: function() {
+    this.timelineHeader
+    .from(this.$title, 1, {autoAlpha: 0, y: -50, ease: Power2.easeOut})
+    .from(this.$c4, 5, {rotationZ: 360, ease: Power2.easeOut}, 'atOneTime')
+    .from(this.$c3, 4, {rotationZ: 360, ease: Power2.easeOut}, 'atOneTime')
+    .from(this.$c5, 4, {rotationZ: 360, ease: Power2.easeOut}, 'atOneTime')
+    .from(this.$item_1, 5, {autoAlpha: 0, x: 250, ease: Power2.easeOut}, 'atOneTime')
+    .from(this.$item_2, 5, {autoAlpha: 0, x: 250, ease: Power2.easeOut}, 'atOneTime')
+    .from(this.$item_6, 5, {autoAlpha: 0, x: 250, ease: Power2.easeOut}, 'atOneTime')
+    .from(this.$item_3, 5, {autoAlpha: 0, x: -500, ease: Power2.easeOut}, 'atOneTime')
+    .from(this.$item_4, 5, {autoAlpha: 0, x: -450, ease: Power2.easeOut}, 'atOneTime')
+    .from(this.$item_5, 5, {autoAlpha: 0, x: -500, ease: Power2.easeOut}, 'atOneTime')
+    .from(this.$btn, 4, {autoAlpha: 0, y: 100, ease: Power2.easeOut}, 'atOneTime')
+
+  }
+}
